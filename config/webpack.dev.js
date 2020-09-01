f38f1d82ae03bc0267bd1d4ac807a736cc7bea35
Mark Arciaga <mark.arciaga@nike.com>
2020-09-01 16:18:22 -0700

    Initial commit

---
+++ config/webpack.dev.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    path: path.join(__dirname, '../build'),
+    filename: '[name].bundle.js'
+  },
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, '../build'),
+    compress: true,
+    port: 3000,
+    overlay: true,
+  },
+  devtool: 'cheap-module-eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+        ]
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            // 'img:src', ':data-src'
+            attributes: {
+              list: [{
+              tag: 'img',
+              attribute: 'data-src',
+              type: 'src',
+            }]},
+            minimize: true,
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
---
+++ config/webpack.prod.js
@@ -0,0 +1,111 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+const glob = require('glob');
+const CompressionPlugin = require('compression-webpack-plugin');
+const BrotliPlugin = require('brotli-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    path: path.join(__dirname, '../build'),
+    filename: '[name].[chunkhash:8].bundle.js',
+    chunkFilename: '[name].[chunkhash:8].chunk.js',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader', // translates CSS into CommonJS
+          'postcss-loader', // Loader for webpack to process CSS with PostCSS
+        ],
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            // 'img:src', ':data-src'
+            attributes: {
+              list: [{
+              tag: 'img',
+              attribute: 'data-src',
+              type: 'src',
+            }]},
+            minimize: true,
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserJSPlugin(),
+      new OptimizeCSSAssetsPlugin(),
+    ],
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      },
+      chunks: 'all'
+    },
+    runtimeChunk: {
+      name: 'runtime'
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new CleanWebpackPlugin(),
+    // This plugin will extract all css to one file
+    new MiniCssExtractPlugin({
+      filename: '[name].[chunkhash:8].bundle.css',
+      chunkFilename: '[name].[chunkhash:8].chunk.css',
+    }),
+    new PurgecssPlugin({
+      paths: glob.sync(path.resolve(__dirname, '../src/**/*'), { nodir: true })
+    }),
+        // ComppresionPlugin will Prepare compressed versions of assets to serve them with Content-Encoding.
+    // In this case we use gzip
+    // But, you can also use the newest algorithm like brotli, and it's supperior than gzip
+    new CompressionPlugin({
+      algorithm: 'gzip',
+    }),
+    new BrotliPlugin(),
+  ],
+};
